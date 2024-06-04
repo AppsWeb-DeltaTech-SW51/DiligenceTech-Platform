b716f327a1012129aaecf60585da6a2ebcbb27f4
Luis Eduardo Herrera Gonzalez <u202218227@upc.edu.pe>
2024-06-04 09:46:57 -0500

    feat: implemented error messages simplified handler for endpoint names

---
+++ DeltaTech.DiligenceTech.API/Shared/Infrastructure/Interfaces/ASP/Configuration/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DeltaTech.DiligenceTech.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+
+/**
+ * <summary>
+ * Extension method to get error messages from a model state dictionary.
+ * Class with method to get error messages from KebabCaseRouteNamingConventions.
+ * </summary>
+ */
+public static class ModelStateExtensions
+{
+    public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+    {
+        return dictionary
+            .SelectMany(m => m.Value!.Errors)
+            .Select(m => m.ErrorMessage)
+            .ToList();
+    }
+}
