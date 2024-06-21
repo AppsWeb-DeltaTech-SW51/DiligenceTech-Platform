8db0471280edb45287a1c8e6117659643b3a95ec
joqui4 <U202211846@upc.edu.pe>
2024-06-20 20:27:20 -0500

    feat: Implemented Project Context Facade

---
+++ DeltaTech.DiligenceTech.API/DueDiligenceProjectManagement/Interfaces/ACL/IProjectsContextFacade.cs
@@ -0,0 +1,8 @@
+﻿namespace DeltaTech.DiligenceTech.API.DueDiligenceProjectManagement.Interfaces.ACL;
+
+public interface IProjectsContextFacade
+{
+    Task<string> CreateProject(string code, string name, bool confirmed);
+
+    Task<string> FetchProjectIdByCode(string code);
+}
---
+++ DeltaTech.DiligenceTech.API/DueDiligenceProjectManagement/Interfaces/ACL/Services/ProjectsContextFacade.cs
@@ -0,0 +1,22 @@
+﻿using DeltaTech.DiligenceTech.API.DueDiligenceProjectManagement.Domain.Model.Commands;
+using DeltaTech.DiligenceTech.API.DueDiligenceProjectManagement.Domain.Model.Queries;
+using DeltaTech.DiligenceTech.API.DueDiligenceProjectManagement.Domain.Services;
+
+namespace DeltaTech.DiligenceTech.API.DueDiligenceProjectManagement.Interfaces.ACL.Services;
+
+public class ProjectsContextFacade(IProjectCommandService projectCommandService, IProjectQueryService projectQueryService) : IProjectsContextFacade
+{
+    public async Task<string> CreateProject(string code, string name, bool confirmed)
+    {
+        var createProjectCommand = new CreateProjectCommand(code, name, confirmed);
+        var project = await projectCommandService.Handle(createProjectCommand);
+        return project?.Code ?? "";
+    }
+
+    public async Task<string> FetchProjectIdByCode(string code)
+    {
+        var getProjectByCodeQuery = new GetProjectByCodeQuery(code);
+        var project = await projectQueryService.Handle(getProjectByCodeQuery);
+        return project?.Code ?? "";
+    }
+}
